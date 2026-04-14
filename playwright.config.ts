555c08b81e37083544560d434f336f3f4b49f44d
Debasmita <b.debasmita@gmail.com>
2026-04-14 23:54:59 +0530

    increased timeout for ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,6 +17,7 @@ dotenv.config({
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+    timeout: 60000,
     /* Run tests in files in parallel */
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
